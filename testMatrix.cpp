c2dc5206407a720e642fc53aa08b3a32744a9630
vm15717 <vm15717@my.bristol.ac.uk>
2025-02-15 22:39:55 +0000

    added a minor test

--- testMatrix.cpp
+++ testMatrix.cpp
@@ -6,8 +6,8 @@ int main()
     std::mt19937 gen(rd());
     Matrix<double> mat1(3,3,1);
     Matrix<double> mat2(3,3,1);
-    mat2.insert(1, 1, 3);
-    mat2.insert(0, 1, 2);
+    mat2.insert(1, 1, 0);
+    mat2.insert(0, 1, 50);
     mat2.insert(2, 2, 0);
     std::string type = "eye";
     Matrix <int> mat5(2, type);
