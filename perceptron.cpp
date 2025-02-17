5f524459ac8b86cce41a30a2a016dd2e4c943cc3
vm15717 <vm15717@my.bristol.ac.uk>
2025-02-17 21:13:05 +0000

    starting to make a perceptron algorithm

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
+#include "Matrix.hpp"
 
 void readCSV(std::string &);
 
 int main()
 {
     std::string filename = "lc.csv";
+    std::vector <double> params;
 }
 
 void parameter_find(std::vector <std::vector <double>> &data)
 {
-    
+    if (data.empty())
+    {
+        throw std::invalid_argument("The data is empty!");
+    }
+    int n = data.size();
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
 }
